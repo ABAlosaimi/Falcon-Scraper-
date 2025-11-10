d9dc0129b7cee71814245e6db4a8bba7ef16d1b1
ABAlosaimi <afalih.alosaimi@gmail.com>
2025-11-11 01:12:21 +0300

    added exception handlers for type mismatch

--- src/main/java/com/proxy/falcon/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/proxy/falcon/Exception/GlobalExceptionHandler.java
@@ -1,9 +1,14 @@
 package com.proxy.falcon.Exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
@@ -21,5 +26,17 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<Object> handleParsePageException(ParsePageException ex) {
         return ResponseEntity.badRequest().body(ex.getMessage());
     }
-    
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Object> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Invalid request body: " + ex.getMostSpecificCause().getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Validation failed: " + ex.getBindingResult().getAllErrors());
+    }
+
 }
